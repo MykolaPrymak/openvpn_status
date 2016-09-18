78af51364054c6222c05d242bffb16c7e7e3ff62
Mykola Prymak <mykola.prymak@gmail.com>
2016-09-18 19:16:29 +0300

    Use connection string instead of configuration object for db

--- openvpn/models/db.js
+++ openvpn/models/db.js
@@ -4,18 +4,6 @@ var config = require('../config');
 var Sequelize = require('sequelize');
 
 
-var sequelize = new Sequelize(config.get('db:name'), config.get('db:options:user'), config.get('db:options:password'), {
-  host: config.get('db:options:host'),
-  dialect: config.get('db:type'),
-  dialectOptions: {
-    socketPath: config.get('db:options:socketPath')
-  },
-
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+var sequelize = new Sequelize(config.get('db'));
 
 module.exports = sequelize;
